94bb29ef1061865449194f46267131bdbc8a0e41
giao <1658773636@qq.com>
2021-12-04 23:09:32 +0800

    修改不需要转换的类

--- postcss.config.js
+++ postcss.config.js
@@ -9,7 +9,7 @@ module.exports = {
       selectorBlackList: ['ignore', 'tab-bar', 'tab-bar-item'], // 指定不需要转换的类
       minPixelValue: 1, // 小于或等于'1px'的不转换为视窗单位
       mediaQuery: false ,// 允许在媒体查询中转换'px'
-      exclude:[/TabBar/]  //不需要转化的组件文件名正则，必须是正则表达式
+      exclude:[/TabBar/]  //不需要转化的组件文件名正则, 必须是正则表达式 
     }
   }
 }
